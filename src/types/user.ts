d0fb49b90234fcd8bafb7fef1ee95b67e60665ae
huyiteng <yitengh2@illinois.edu>
2025-12-16 05:50:32 +0800

    add optional compensation attribute in task

--- src/types/user.ts
+++ src/types/user.ts
@@ -57,6 +57,10 @@ export interface Task {
    * Optional numeric position for drag-and-drop (for p2p sync)
    */
   position?: number;
+  /**
+   * freelancer related
+   */
+  compensation?: Money;
 }
 
 /**
@@ -93,3 +97,7 @@ export interface AppSettings {
 
 export type SortOption = "dateCreated" | "dueDate" | "alphabetical" | "custom";
 export type ReduceMotionOption = "system" | "on" | "off";
+export type Money = {
+  amount: number;
+  currency: "CNY" | "USD";
+};
