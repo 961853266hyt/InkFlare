cbe3d93c847a12718e7c88480fb26b5afd0b1295
huyiteng <yitengh2@illinois.edu>
2026-01-05 06:45:53 +0800

    Revert "fix: handle promise"
    This reverts commit 8b0364bb7bed7f3f6dec3a465743549302e2ff7b.

--- src/components/settings/LanguageSwitch.tsx
+++ src/components/settings/LanguageSwitch.tsx
@@ -10,7 +10,7 @@ const LanguageSwitch: React.FC = () => {
 
   const isZhCN = user.settings.language === "zh-CN";
 
-  const handleToggle = async () => {
+  const handleToggle = () => {
     const newLanguage: Language = isZhCN ? "en" : "zh-CN";
 
     const updatedSettings = {
@@ -19,7 +19,7 @@ const LanguageSwitch: React.FC = () => {
     };
     setUser((prev) => ({ ...prev, settings: updatedSettings }));
 
-    await i18n.changeLanguage(newLanguage);
+    i18n.changeLanguage(newLanguage);
     document.documentElement.lang = newLanguage;
   };
 
