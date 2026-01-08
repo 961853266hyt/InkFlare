f32dc42085d62a9398dad632477e63a417d9bcb2
huyiteng <yitengh2@illinois.edu>
2026-01-09 02:26:45 +0800

    feat: Enhance Presets page with AddView component and section heading

--- src/pages/Presets.tsx
+++ src/pages/Presets.tsx
@@ -1,5 +1,27 @@
+import { SectionHeading } from "../components/settings/settings.styled.tsx";
+
+import { AddRounded } from "@mui/icons-material";
+import { Button } from "@mui/material";
+
+// const data: Platform =
+
+const AddView = () => {
+  return (
+    <div>
+      <Button variant="outlined" sx={{ height: "80", width: "80", mt: 2 }} color="primary">
+        <AddRounded style={{ fontSize: "44px" }} />
+      </Button>
+    </div>
+  );
+};
+
 const Presets = () => {
-  return <div>Presets Page</div>;
+  return (
+    <div>
+      <SectionHeading>接单平台</SectionHeading>
+      <AddView />
+    </div>
+  );
 };
 
 export default Presets;
