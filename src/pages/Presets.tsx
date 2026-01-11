677b8a60a529a6b4d30b1570c0a2e7d43c336dd8
huyiteng <yitengh2@illinois.edu>
2026-01-11 22:42:46 +0800

    feat: Implement CreatePlatformView dialog and integrate with AddView component

--- src/pages/Presets.tsx
+++ src/pages/Presets.tsx
@@ -1,17 +1,67 @@
 import { TabHeading } from "../components/settings/settings.styled.tsx";
 
 import { AddRounded } from "@mui/icons-material";
-import { Button } from "@mui/material";
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from "@mui/material";
 import { TopBar } from "../components";
 // import { ManagementHeader } from "../styles";
 import { useTranslation } from "react-i18next";
+import { useState } from "react";
 
 // const data: Platform =
+interface CreatePlatformViewProps {
+  isOpen: boolean;
+  onClose: () => void;
+  handleCloseButtonClick?: () => void;
+}
 
-const AddView = () => {
+const CreatePlatformView = ({
+  onClose,
+  isOpen,
+  handleCloseButtonClick,
+}: CreatePlatformViewProps) => {
+  return (
+    <Dialog
+      open={isOpen}
+      onClose={onClose}
+      // maxWidth="sm" // 可以设置最大宽度
+      // fullWidth    // 配合 maxWidth 使用
+    >
+      <DialogTitle>提示</DialogTitle>
+      <DialogContent>
+        <DialogContentText>这是一个默认在屏幕正中央的弹窗。</DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleCloseButtonClick}>关闭</Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
+
+interface AddViewProps {
+  onClose: () => void;
+}
+const AddView = ({ onClose }: AddViewProps) => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <div>
-      <Button variant="outlined" sx={{ height: "80", width: "80", mt: 2 }} color="primary">
+      <CreatePlatformView
+        onClose={onClose}
+        isOpen={isOpen}
+        handleCloseButtonClick={() => setIsOpen(false)}
+      />
+      <Button
+        variant="outlined"
+        sx={{ height: "80", width: "80", mt: 2 }}
+        color="primary"
+        onClick={() => setIsOpen(true)}
+      >
         <AddRounded style={{ fontSize: "44px" }} />
       </Button>
     </div>
@@ -20,12 +70,13 @@ const AddView = () => {
 
 const Presets = () => {
   const { t } = useTranslation();
+
   return (
     <div>
       <TopBar title={t("common.presets")} />
-      {/*<ManagementHeader>{t("presets.header")}</ManagementHeader>*/}
       <TabHeading>常用平台</TabHeading>
-      <AddView />
+
+      <AddView onClose={() => {}} />
     </div>
   );
 };
